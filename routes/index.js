13561bebb6f0defe931ecdd6b8938d53b6373fc8
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2022-10-05 18:53:53 +0530

    mon data

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  const values = ["vishakh", "sundeep", "vikram", "arun"];
-  res.render("index", { values });
+  // const values = ["vishakh", "sundeep", "vikram", "arun"];
+  const person = {
+    name: "vishakh",
+    comments: { comment: "am a monster", date: "7-9-2004" },
+  };
+  res.render("index", { person });
 });
 
 module.exports = router;
